5ae7285151e9d2560caa69bda2d217b204e995b1
Safia <aifasshire@gmail.com>
2021-01-18 01:25:45 +0000

    working GET request

--- server/routes/bootcampsRoutes.js
+++ server/routes/bootcampsRoutes.js
@@ -1,39 +1,48 @@
-const mongoose = require('mongoose');
-const Bootcamp = mongoose.model('bootcamps');
+const mongoose = require("mongoose");
+const Bootcamp = mongoose.model("bootcamps");
 
 module.exports = (app) => {
+
+
   app.get(`/api/bootcamp`, async (req, res) => {
-    const results = await Bootcamp.find();
-    return res.status(200).send(results);
-  });
+    const query = req.query.q;
+    console.log(query);
+    const results = await Bootcamp.find({company:query});
+    console.log(results);
 
-  app.post(`/api/bootcamp`, async (req, res) => {
-    const bootcamp = await Bootcamp.create(req.body);
-    return res.status(201).send({
-      error: false,
-      bootcamp,
+    res.json({
+      bootcamp: results,
     });
   });
 
-  app.put(`/api/bootcamp/:id`, async (req, res) => {
-    const { id } = req.params;
 
-    const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body);
+  // app.post(`/api/bootcamp`, async (req, res) => {
+  //   const bootcamp = await Bootcamp.create(req.body);
+  //   return res.status(202).send({
+  //     error: false,
+  //     bootcamp,
+  //   });
+  // });
 
-    return res.status(202).send({
-      error: false,
-      bootcamp,
-    });
-  });
+  // app.put(`/api/bootcamp/:id`, async (req, res) => {
+  //   const { id } = req.params;
 
-  app.delete(`/api/bootcamp/:id`, async (req, res) => {
-    const { id } = req.params;
+  //   const bootcamp = await Bootcamp.findByIdAndUpdate(id, req.body);
 
-    const bootcamp = await Bootcamp.findByIdAndDelete(id);
+  //   return res.status(203).send({
+  //     error: false,
+  //     bootcamp,
+  //   });
+  // });
 
-    return res.status(202).send({
-      error: false,
-      bootcamp,
-    });
-  });
+  // app.delete(`/api/bootcamp/:id`, async (req, res) => {
+  //   const { id } = req.params;
+
+  //   const bootcamp = await Bootcamp.findByIdAndDelete(id);
+
+  //   return res.status(204).send({
+  //     error: false,
+  //     bootcamp,
+  //   });
+  // });
 };
