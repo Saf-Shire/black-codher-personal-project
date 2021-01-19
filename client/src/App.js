d4f419b62399a5bccecc7d04b109ecd61b694cac
Safia <aifasshire@gmail.com>
2021-01-19 22:55:03 +0000

    addBootcamp function created and Header moved above Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Bootcamplist from './components/Bootcamplist';
+import React, { useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
 
-
-// SERVICES
-import userService from './services/userService';
+import "./stylesheets/App.css";
+import About from "./pages/About";
+import Footer from "./components/Footer";
+import Filter from "./components/Filter";
+import SavedList from "./components/SavedList";
+import Resources from "./pages/Resources";
 
 function App() {
-  const [bootcamps, setbootcamps] = useState([]);
-
-  useEffect(() => {
-  console.log(typeof bootcamps)
-    if (Array.isArray(bootcamps) && bootcamps.length ==0)  {
-        console.log(bootcamps )
-      getbootcamps();
-    }
-  });
-
-  const getbootcamps = async () => {
-    let res = await userService.getAll();
-    let result=res.map(item => {
-      // delete item.location.city
-      return item
-    })
-    console.log(typeof res);
-    console.log("test");
-    setbootcamps(res);
-};
-
- const renderBootcamp= (bootcamp) =>{
-     return (
-      <div>
-        <li>
-          <h3>
-            {bootcamp.title}-{bootcamp.company}
-          </h3>
-          {bootcamp.location.city[0]}
-        </li>  
-      </div>
-    );
+  const [savedbootcamp, setSavedBootcamp] = useState([]);
+  function addBootcamp(bootcamp) {
+    console.log(bootcamp);
+    setSavedBootcamp([...savedbootcamp, ...bootcamp]);
   }
-  
+  function removeBootcamp() {
+    setSavedBootcamp([]);
+  }
+
   return (
-        <div>
-          <ul>
-            {bootcamps && bootcamps.length > 0 ? (
-              bootcamps.map((bootcamp) => renderBootcamp(bootcamp))
-            ) : (
-              <p>No Bootcamps found</p>
-            )}
-          </ul>
-        </div>
-      );
-      
- 
-return(
+    <BrowserRouter>
+    <Header/>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => (
+            <React.Fragment>
+              {/* <Filter count={bootcamps.length}/> */}
+              <About />
+              <LandingPage addBootcamp={addBootcamp} />
+              <Footer />
+            </React.Fragment>
+          )}
+        />
 
-<BrowserRouter>
-<Switch>       
-<Route exact path="/" render={() => (
-<React.Fragment>
-<Bootcamplist bootcamps={bootcamps}/>
- </React.Fragment>
-
-                  )} />
- 
-
-                   
-                 
-                  </Switch>
-              </BrowserRouter>
-
-)
-              
+        <Route
+          exact
+          path="/saved"
+          render={() => (
+            <React.Fragment>
+              <SavedList
+                savedBootcamp={savedbootcamp}
+                removeBootcamp={removeBootcamp}
+              />
+            </React.Fragment>
+          )}
+        />
+        <Route
+          exact
+          path="/resources"
+          render={() => (
+            <React.Fragment>
+              <Resources/>
+            </React.Fragment>
+          )}
+        />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 export default App;
