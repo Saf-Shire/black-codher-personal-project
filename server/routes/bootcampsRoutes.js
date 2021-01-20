fb0dad35a7836d5efb753371d1cd433d4dec361d
Safia <aifasshire@gmail.com>
2021-01-20 20:51:32 +0000

    query changed to case insensitive

--- server/routes/bootcampsRoutes.js
+++ server/routes/bootcampsRoutes.js
@@ -1,21 +1,36 @@
+const { query } = require("express");
 const mongoose = require("mongoose");
 const Bootcamp = mongoose.model("bootcamps");
 
 module.exports = (app) => {
   app.get(`/api/bootcamp`, async (req, res) => {
+    console.log(req.query);
     let searchOptions = {};
-    if (req.query.company != null && req.query.company !== "") {
-      searchOptions.company = new RegExp(req.query.company, "i");
+    let q = req.query.q;
+    if (q != null && q!== "undefined" && q !== "") {
+      searchOptions.company = new RegExp(q, "i");
     }
     try {
       const results = await Bootcamp.find(searchOptions);
-      console.log(results);
+      // console.log(results);
       res.json({
         bootcamp: results,
+        searchOptions:q
       });
     } catch {
       res.redirect("/");
     }
+
+    // app.get(`/api/bootcamp`, async (req, res) => {
+    //   const query = req.query.q;
+    //   console.log(query);
+    //   const results = await Bootcamp.find({company:query});
+    //   console.log(results);
+  
+    //   res.json({
+    //     bootcamp: results,
+    //   });
+
   });
 
 };
