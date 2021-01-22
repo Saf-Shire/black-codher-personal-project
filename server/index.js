20d524d8e3f66640c4e60a4e1c22b3ae79f509ac
Safia <aifasshire@gmail.com>
2021-01-22 15:06:57 +0000

    cors added as middleware

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
+cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 // IMPORT YOUR MODELS
 require("./models/Bootcamps");
 
+
 const app = express();
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  next();
-});
+app.use(cors());
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   next();
+// });
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI ||
@@ -24,6 +27,7 @@ app.use(bodyParser.json());
 
 // IMPORT YOUR ROUTES
 require("./routes/bootcampsRoutes")(app);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
