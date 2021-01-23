d397840fbc69490a25887fc1062fcb9f269d28d9
Safia <aifasshire@gmail.com>
2021-01-23 14:30:11 +0000

    Services for filter component

---
+++ client/src/services/bootcampService.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-anonymous-default-export */
+import axios from 'axios';
+
+export default {
+  getAll: async () => {
+    const res = await axios.get(`/api/bootcamp`);
+    return res.data || [];
+  },
+};
