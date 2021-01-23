016e6a6f89475ea8794070105b16f9327d68e004
Safia <aifasshire@gmail.com>
2021-01-23 14:29:10 +0000

    moved out of landing page

---
+++ client/src/pages/About.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Container } from 'react-bootstrap';
+
+const About = () => {
+
+    // }
+    return ( 
+       <React.Fragment>
+           <Container fluid className="about">
+               <div>
+                   <h1>TechitOn</h1>
+                   <p>
+                       Start your journey to become a web developer by searching for the bootcamp that suits you.
+                    
+
+                   </p>
+               </div>
+
+           </Container>
+       </React.Fragment> 
+     );
+}
+ 
+export default About ;
