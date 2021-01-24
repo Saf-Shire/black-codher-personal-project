d3f05efcec25ce8184a528047a25374eced72ddc
Safia <aifasshire@gmail.com>
2021-01-24 04:53:50 +0000

    Formatted CSS and added icon button

--- client/src/components/Practice.js
+++ client/src/components/Practice.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import practices from "../data/practice.json";
+import Button from "@material-ui/core/Button";
+import SynchIcon from "@material-ui/icons/Sync"
 
 const Practice = () => {
   return (
@@ -9,13 +11,23 @@ const Practice = () => {
         practices.map((practice) => (
           <Card className="card">
             <div id="card-style" key={practice.name}>
-              <h3>{practice.name}</h3>
               <Card.Img
                 src={practice.imageLink}
                 alt={practice.name}
                 className="img"
               />
-              <p> {practice.webLink} </p>
+              <Card.Body id="card-body">
+                <Card.Text className="card-title">{practice.name}</Card.Text>
+                <Button
+                  className="link-btn"
+                  href={practice.webLink}
+                  target="_blank"
+                  size="large"
+                  endIcon={<SynchIcon />}
+                >
+                  Practice
+                </Button>
+              </Card.Body>
             </div>
           </Card>
         ))
