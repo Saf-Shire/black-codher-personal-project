ffed775a20eb33a34f89dfdbf38d714b61e6ceb7
Safia <aifasshire@gmail.com>
2021-01-24 23:48:05 +0000

    Added AOS animation

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
 import LandingPage from "./components/LandingPage";
-
+import AOS from "aos";
+import "aos/dist/aos.css";
 import "./stylesheets/App.css";
 import Jumbotron from "./components/Jumbotron"
 import Copyright from "./components/Copyright";
@@ -18,6 +19,10 @@ function App() {
   function removeBootcamp() {
     setSavedBootcamp([]);
   }
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
 
   return (
     <BrowserRouter>
