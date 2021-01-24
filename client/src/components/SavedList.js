111004c917588eebdf2d94fd339e254d293c670e
Safia <aifasshire@gmail.com>
2021-01-24 23:52:23 +0000

    Changed card styling

--- client/src/components/SavedList.js
+++ client/src/components/SavedList.js
@@ -1,6 +1,13 @@
 import React, { useState } from "react";
-import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
+import Button from "@material-ui/core/Button";
+import RemoveCircleOutlineIcon from '@material-ui/icons/RemoveCircleOutline';
+import LanguageIcon from "@material-ui/icons/Language";
+import TimelapseIcon from "@material-ui/icons/Timelapse";
+import LocationIcon from "@material-ui/icons/LocationOn";
+import CreditCardIcon from "@material-ui/icons/CreditCard";
+import RotateRightOutlinedIcon from "@material-ui/icons/RotateRightOutlined";
+import ContactMailOutlinedIcon from "@material-ui/icons/ContactMailOutlined";
 
 const SavedList = (props, { removeBootcamp }) => {
   const [savedlist, setSavedlist] = useState([]);
@@ -17,41 +24,60 @@ const SavedList = (props, { removeBootcamp }) => {
     <React.Fragment>
       <div>
         {props.savedBootcamp.map((bootcamp) => (
-          <Card className="search-result">
-            <div className="result-style" key={bootcamp._id}>
+            <Card className="card">
+            <div id="card-style" key={bootcamp._id}>
               {
                 <Card.Img
                   src={bootcamp.logo}
-                  alt="bootcampimage"
+                  alt={bootcamp.company}
                   className="img"
                 />
               }
-              <Card.Body className="body">
-                <Card.Title class="title">
-                  Title:{" "}
-                  <span class="font-normal text-base leadin-relaxed">
-                    {bootcamp.title}
+              <Card.Body id="card-body">
+                <Card.Text className="card-title">
+                  {bootcamp.company} - {bootcamp.title}
+                </Card.Text>
+                <Card.Text className="card-text">
+                  <LocationIcon />
+                  <span className="span-text">{bootcamp.city}</span>
+                </Card.Text>
+                <Card.Text className="card-text">
+                  <TimelapseIcon />
+                  <span className="span-text">
+                    {bootcamp.durationType}| {bootcamp.weeksLength} weeks |{" "}
+                    {bootcamp.totalAmountOfHours} hours
                   </span>
-                </Card.Title>
-                <p class="font-medium text-lg">
-                  Company:{" "}
-                  <span class="font-normal text-base">{bootcamp.company}</span>
-                </p>
-                <p class="font-medium text-lg">
-                  Location:{" "}
-                  <span class="font-normal text-base 1">{bootcamp.city}</span>
-                </p>
-                <p class="font-medium text-lg">
-                  Duration:{" "}
-                  <span class="font-normal text-base 1">
-                    {bootcamp.durationType}
+                </Card.Text>
+                <Card.Text className="card-text">
+                  <CreditCardIcon />
+                  <span className="span-text">£{bootcamp.tuitionFee}</span>
+                </Card.Text>
+                <Card.Text className="card-text">
+                  <RotateRightOutlinedIcon />
+                  <span className="span-text">
+                    {bootcamp.applicationCycles} application cycle(s)
                   </span>
-                </p>
+                </Card.Text>
+                <Card.Text className="card-text">
+                  <ContactMailOutlinedIcon />
+                  <span className="span-text">{bootcamp.emailAddress}</span>
+                </Card.Text>
                 <Button
-                  id="controlButtons"
-                  onClick={() => unfavouriteBootcamp(bootcamp._id)}
+                    id="controlButtons"
+                    onClick={() => unfavouriteBootcamp(bootcamp._id)}
+                    size="large"
+                    endIcon={<RemoveCircleOutlineIcon/>}
+                  >
+                    Remove
+                  </Button>
+                <Button
+                  className="link-btn"
+                  href={bootcamp.applicationLink}
+                  target="_blank"
+                  size="large"
+                  endIcon={<LanguageIcon />}
                 >
-                  Remove -
+                  Apply
                 </Button>
               </Card.Body>
             </div>
