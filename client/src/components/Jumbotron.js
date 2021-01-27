bf787430acb400f706ea3e628c33fd7fed405c08
Safia <aifasshire@gmail.com>
2021-01-27 02:17:19 +0000

    Removed navigation buttons

--- client/src/components/Jumbotron.js
+++ client/src/components/Jumbotron.js
@@ -2,40 +2,22 @@ import React from "react";
 import { Jumbotron as Jumbo, Container } from "react-bootstrap";
 import "../stylesheets/App.css";
 
-// import { Link } from "react-router-dom";
-// import Button from "@material-ui/core/Button";
-// import SearchIcon from "@material-ui/icons/Search";
-// import { useHistory } from "react-router";
+
+import { useHistory } from "react-router";
 
 const Jumbotron = () => {
-  // const { push } = useHistory();
   return (
-    <Jumbo fluid className="jumbo" data-aos="fade-up-right" data-aos-duration="2000"
-    >
+    <Jumbo fluid className="jumbo" data-aos="fade-up" data-aos-duration="2000">
       <div className="overlay"></div>
       <Container>
         <div>
-          <h1 data-aos="zoom-in-up">
+          <h1 data-aos="zoom-in-up" >
             <strong>TechitOn</strong>
           </h1>
-          <h2>
-            {" "}
+          <h2 data-aos="zoom-out" data-aos-delay="200">
             Start your journey to become a web developer by searching for the
-            bootcamp that suits you.{" "}
+            bootcamp that suits you.
           </h2>
-{/* 
-          <Button
-            variant="contained"
-            className="link-btn"
-            size="large"
-            endIcon={<SearchIcon />}
-            onClick={() => push("/search")}
-          >
-            Browse course catalog
-          </Button>
-          <Link to="/search" exact>
-            Browse{" "}
-          </Link> */}
         </div>
       </Container>
     </Jumbo>
