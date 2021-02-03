8df45c934a4890b353de7a3d4a545fef2cfd4ada
Safia <aifasshire@gmail.com>
2021-02-03 22:44:49 +0000

    Adding a path

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 cors = require('cors');
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const app = express();
 // IMPORT YOUR MODELS
 require("./models/Bootcamps");
 
 
-const app = express();
+
 app.use(cors());
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", "*");
@@ -28,6 +28,14 @@ app.use(bodyParser.json());
 // IMPORT YOUR ROUTES
 require("./routes/bootcampsRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
