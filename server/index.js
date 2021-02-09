25f7a74b44c9118d9f817cb233532c2abca1b9d3
Safia <aifasshire@gmail.com>
2021-02-09 16:45:37 +0000

    express change

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-cors = require('cors');
+cors = require("cors");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
 // IMPORT YOUR MODELS
 require("./models/Bootcamps");
-
-
 
 app.use(cors());
 // app.use((req, res, next) => {
@@ -14,21 +12,20 @@ app.use(cors());
 //   next();
 // });
 mongoose.Promise = global.Promise;
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    `mongodb://localhost:27017/blackcodher`,
-  {
+mongoose
+  .connect(process.env.MONGODB_URI || `mongodb://localhost:27017/blackcodher`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }
-)
-.then(() => console.log("Connection Successful"))
-    .catch(err => console.log(err));
+  })
+  .then(() => console.log("Connection Successful"))
+  .catch((err) => console.log(err));
 
 app.use(bodyParser.json());
 
 // IMPORT YOUR ROUTES
 require("./routes/bootcampsRoutes")(app);
+
+const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -39,7 +36,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
